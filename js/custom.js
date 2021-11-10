ab792d5e34f473b046079f5fabe22e4be3c6ca28
d.fomenko <d.fomenko@viseven.com>
2021-11-10 12:22:52 +0200

    main

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// $('.customer-coments-container').slick();
+
+$('.customer-coments-slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnDotsHover: true,
+    // variableWidth: true
+});
