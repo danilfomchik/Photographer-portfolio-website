695215b2c499e6a4b0e1e257e2f84b00be5bfc0d
d.fomenko <d.fomenko@viseven.com>
2021-11-15 09:07:41 +0200

    main

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,18 @@
-// $('.customer-coments-container').slick();
+const menuBtn = document.getElementById('menu-btn');
 
+menuBtn.addEventListener('click', function (e){
+    e.preventDefault();
+    this.classList.toggle('menu-btn_active');
+})
+
+
+
+
+
+
+
+
+//slick slider
 $('.customer-coments-slick').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
